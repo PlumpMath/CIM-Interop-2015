1ac026a3c3d0b5f8b56054d738cafdc44e4fff80
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2015-06-04 10:40:02 +0100

    .NET subsidiary system - barebones of project

---
+++ src/mock-subsidiary/DotNetSecondaryCareSystem/SecondaryCareSystemForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace DotNetSecondaryCareSystem
+{
+    partial class SecondaryCareSystemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SecondaryCareSystemForm));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbCimUrl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 1);
+            this.panel2.Size = new System.Drawing.Size(1617, 93);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbCimUrl);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1617, 92);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "For the Common Interface Mechanism Project";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(599, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Secondary Care System Demonstrator (CIM Consumer)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "6936548122";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 245);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(806, 422);
+            this.textBox2.TabIndex = 5;
+            // 
+            // tbCimUrl
+            // 
+            this.tbCimUrl.Location = new System.Drawing.Point(1322, 51);
+            this.tbCimUrl.Name = "tbCimUrl";
+            this.tbCimUrl.Size = new System.Drawing.Size(260, 23);
+            this.tbCimUrl.TabIndex = 6;
+            this.tbCimUrl.Text = "http://localhost:8080/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1252, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CIM URL";
+            // 
+            // SecondaryCareSystemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1617, 860);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "SecondaryCareSystemForm";
+            this.Text = "Secondary Care System Demonstrator v0.1";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCimUrl;
+    }
+}
+
---
+++ src/mock-subsidiary/DotNetSecondaryCareSystem/SecondaryCareSystemForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetSecondaryCareSystem
+{
+    public partial class SecondaryCareSystemForm : Form
+    {
+        public SecondaryCareSystemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbCimUrl.Text + 
+
+            var webRequest = (HttpWebRequest)WebRequest.Create("http://endeavour-cim.cloudapp.net/CIM_war/v0.1/service/21/patient/5ec13d3e-cd1c-42b0-9203-fac7d7440c70");
+            webRequest.Method = "GET";
+            webRequest.Headers.Add("api_key", "swagger");
+            webRequest.Headers.Add("hash", generateHash("/service/{serviceId}/patient/{patientId}", null));
+
+            var webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            Stream responseStream = webResponse.GetResponseStream();
+            String response;
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+                response = reader.ReadToEnd();
+            }
+            Debug.WriteLine(response);
+        }
+
+        private static String generateHash(String method, String body)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            String privateKey = "privateKey";
+
+            String data = method;
+            if (data != null)
+                data += body;
+
+            HMAC mac = HMAC.Create("HmacSHA256");
+            mac.Key = encoder.GetBytes(privateKey);
+            mac.Initialize();
+            byte[] digest = mac.ComputeHash(encoder.GetBytes(data));
+            String hash = Convert.ToBase64String(digest);
+            return hash;
+        }
+    }
+}
