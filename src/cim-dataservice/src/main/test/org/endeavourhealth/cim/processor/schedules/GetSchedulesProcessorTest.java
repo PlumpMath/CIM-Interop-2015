2b1c5bc16cfd5e562b6597ca18577aeb7f0e591a
ErgoSoftUK <ergosoftuk@gmail.com>
2015-06-25 17:01:27 +0100

    Added CAMEL unit tests for Schedules processor

---
+++ src/cim-dataservice/src/main/test/org/endeavourhealth/cim/processor/schedules/GetSchedulesProcessorTest.java
@@ -0,0 +1,115 @@
+package org.endeavourhealth.cim.processor.schedules;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetSchedulesProcessorTest extends CamelTestSupport {
+	@EndpointInject(uri = "mock:result")
+	protected MockEndpoint resultEndpoint;
+
+	@Produce(uri = "direct:start")
+	protected ProducerTemplate template;
+
+	@Override
+	protected RouteBuilder createRouteBuilder() {
+		return new RouteBuilder() {
+			public void configure() {
+				from("direct:start")
+					.process(new GetSchedulesProcessor())
+					.to("mock:result");
+			}
+		};
+	}
+
+	@Test(expected = Exception.class)
+	public void testProcessNoParams() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", null);
+		headerParams.put("date", null);
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
+	@Test(expected = Exception.class)
+	public void testProcessBothParams() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", "practitioner|G12345");
+		ArrayList<String> dates = new ArrayList<>();
+		dates.add("2015-06-30T00:00:00Z");
+		headerParams.put("date", dates);
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
+	@Test(expected = Exception.class)
+	public void testProcessWrongDateType() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", null);
+		headerParams.put("date", "2015-06-22T00:00:00Z");
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
+	@Test(expected = Exception.class)
+	public void testProcessNotEnoughDates() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", null);
+		ArrayList<String> dates = new ArrayList<>();
+		dates.add("2015-06-22T00:00:00Z");
+		headerParams.put("date", dates);
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
+	@Test(expected = Exception.class)
+	public void testProcessToManyDates() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", null);
+		ArrayList<String> dates = new ArrayList<>();
+		dates.add("2015-06-22T00:00:00Z");
+		dates.add("2015-06-25T00:00:00Z");
+		dates.add("2015-06-30T00:00:00Z");
+		headerParams.put("date", dates);
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
+	@Test
+	public void testProcessSuccess() throws Exception {
+		Map<String, Object> headerParams = new HashMap<>();
+		headerParams.put("odsCode", "A99999");
+		headerParams.put("actor", null);
+		ArrayList<String> dates = new ArrayList<>();
+		dates.add("2015-06-22T00:00:00Z");
+		dates.add("2015-06-30T00:00:00Z");
+		headerParams.put("date", dates);
+
+		template.sendBodyAndHeaders(null, headerParams);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+}
