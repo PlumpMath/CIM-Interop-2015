bc9d783b917d1bb015226e9ce3cf9d38ee7b7923
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2015-08-11 21:08:14 +0100

    Further Swagger changes: Move adding of Endeavour Logo into custom scripts, disable Curl output, temporarily disable Hash textbox

--- src/cim-apidoc/web/swagger-custom/script.js
+++ src/cim-apidoc/web/swagger-custom/script.js
@@ -1,13 +1,37 @@
-function visualizeJson(classId)
-{
-	var jsonString = $('#'+classId).find('code').text();
-  var json = JSON.parse(jsonString);
+$(document).ready(function() {
 
-  popupWin = window.open('swagger-localised/jsonVisualizer.html');
-  popupWin.focus();
+  addCustomLogo();
+  removeCurl();
+  disableHash(); // temporarily
 
-  popupWin.addEventListener('load', function(){
-		popupWin.visualize(json);
+  function addCustomLogo() {
+    if ($('#custom-logo').length == 0) {
+      $('<a href="/"><div id="custom-logo"></div></a>').insertBefore('#logo');
+    }
+  }
+
+  function removeCurl() {
+    $('h4').filter(function() {
+      return $(this).text() == 'Curl'
+    }).css('display', 'none');
+
+    $('.block.curl').css('display', 'none');
+  }
+
+  function disableHash() {
+    $('input[name=hash]').attr("disabled", "disabled");
+  }
+
+  function visualizeJson(classId)
+  {
+    var jsonString = $('#'+classId).find('code').text();
+    var json = JSON.parse(jsonString);
+
+    popupWin = window.open('swagger-custom/jsonVisualizer.html');
+    popupWin.focus();
+
+    popupWin.addEventListener('load', function(){
+      popupWin.visualize(json);
     }, true);
-}
-		
+  }
+});
