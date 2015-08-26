d76ff764526992d4a60b1fcd4ab3043fda50320a
ErgoSoftUK <ergosoftuk@gmail.com>
2015-08-26 16:11:37 +0100

    Attempt to fix tomcat leak/shutdown issue with Cassandra

---
+++ src/cim-dataservice/src/main/java/org/endeavourhealth/cim/CimDataServiceContextListener.java
@@ -0,0 +1,24 @@
+package org.endeavourhealth.cim;
+
+import org.endeavourhealth.cim.common.repository.common.data.DbClient;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class CimDataServiceContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		System.out.println("Starting up!");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		System.out.print("Closing Cassandra connections...");
+		DbClient dbClient = DbClient.getInstance();
+		if (dbClient != null)
+			dbClient.close();
+
+		System.out.println("...done.");
+	}
+}
