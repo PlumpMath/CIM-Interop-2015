6b3be686f869bb849ad093a9a7137db6c19efb8d
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-02-16 16:59:14 +0000

    Show message on uncaught exceptions in Secondard Care Demonstrator

--- src/mock-systems/DotNetConsumerSystem/DotNetSecondaryCareSystem/Program.cs
+++ src/mock-systems/DotNetConsumerSystem/DotNetSecondaryCareSystem/Program.cs
@@ -16,7 +16,21 @@ namespace DotNetSecondaryCareSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += Application_ThreadException;
             Application.Run(new SecondaryCareSystemForm());
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Exception:  " + e.Exception.GetType().FullName + Environment.NewLine
+                + Environment.NewLine
+                + "Message:  " + e.Exception.Message + Environment.NewLine
+                + Environment.NewLine
+                + "Stack trace:  " + Environment.NewLine
+                + e.Exception.StackTrace,
+                "Exception",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning);
         }
     }
 }
