00276b6578f314a650e74ff943ea2436a87a2df9
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-02-16 17:00:51 +0000

    Complete immunisation transform

--- src/cim-core/src/main/java/org/endeavourhealth/cim/transform/openhr/tofhir/clinical/ImmunisationTransformer.java
+++ src/cim-core/src/main/java/org/endeavourhealth/cim/transform/openhr/tofhir/clinical/ImmunisationTransformer.java
@@ -1,16 +1,56 @@
 package org.endeavourhealth.cim.transform.openhr.tofhir.clinical;
 
+import org.apache.commons.lang3.StringUtils;
+import org.endeavourhealth.cim.transform.common.ReferenceHelper;
+import org.endeavourhealth.cim.transform.exceptions.SourceDocumentInvalidException;
 import org.endeavourhealth.cim.transform.exceptions.TransformException;
 import org.endeavourhealth.cim.transform.openhr.tofhir.FHIRContainer;
+import org.endeavourhealth.cim.transform.openhr.tofhir.ToFHIRHelper;
+import org.endeavourhealth.cim.transform.schemas.openhr.DtDatePart;
+import org.endeavourhealth.cim.transform.schemas.openhr.OpenHR001Encounter;
 import org.endeavourhealth.cim.transform.schemas.openhr.OpenHR001HealthDomain;
-import org.hl7.fhir.instance.model.Immunization;
+import org.hl7.fhir.instance.model.*;
 
 class ImmunisationTransformer implements ClinicalResourceTransformer {
     public Immunization transform(OpenHR001HealthDomain healthDomain, FHIRContainer container, OpenHR001HealthDomain.Event source) throws TransformException
     {
         Immunization target = new Immunization();
         target.setId(source.getId());
-
+        target.setStatus(MedicationAdministration.MedicationAdministrationStatus.COMPLETED.toCode());
+        target.setDateElement(convertEffectiveDateTime(source.getEffectiveTime()));
+        target.setPatient(convertPatient(source.getPatient()));
+        target.setPerformer(convertUserInRole(source.getAuthorisingUserInRole()));
+        target.setEncounter(getEncounter(container, source.getId()));
+        target.setVaccineCode(CodeHelper.convertCode(source.getCode(), source.getDisplayTerm()));
         return target;
     }
+
+    private DateTimeType convertEffectiveDateTime(DtDatePart source) throws TransformException {
+        if (source == null)
+            throw new SourceDocumentInvalidException("Invalid DateTime");
+
+        return ToFHIRHelper.convertPartialDateTimeToDateTimeType(source);
+    }
+
+    private Reference convertPatient(String sourcePatientId) throws SourceDocumentInvalidException {
+        if (StringUtils.isBlank(sourcePatientId))
+            throw new SourceDocumentInvalidException("Invalid Patient Id");
+        return ReferenceHelper.createReference(ResourceType.Patient, sourcePatientId);
+    }
+
+    private Reference convertUserInRole(String userInRoleId) throws SourceDocumentInvalidException {
+        if (StringUtils.isBlank(userInRoleId))
+            throw new SourceDocumentInvalidException("UserInRoleId not found");
+
+        return ReferenceHelper.createReference(ResourceType.Practitioner, userInRoleId);
+    }
+
+    private Reference getEncounter(FHIRContainer container, String eventId) {
+        OpenHR001Encounter encounter = container.getEncounterFromEventId(eventId);
+        if (encounter == null)
+            return null;
+
+        return ReferenceHelper.createReference(ResourceType.Encounter, encounter.getId());
+    }
+
 }
