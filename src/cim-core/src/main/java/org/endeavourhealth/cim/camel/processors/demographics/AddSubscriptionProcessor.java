093013c682c00efccfca891dfaeb1b9c4e7badc6
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-02-23 22:40:23 +0000

    Disable subscription processing during connectathon

--- src/cim-core/src/main/java/org/endeavourhealth/cim/camel/processors/demographics/AddSubscriptionProcessor.java
+++ src/cim-core/src/main/java/org/endeavourhealth/cim/camel/processors/demographics/AddSubscriptionProcessor.java
@@ -9,9 +9,9 @@ public class AddSubscriptionProcessor implements org.apache.camel.Processor {
 
     public void process(Exchange exchange) throws Exception {
 
-        String odsCode = ExchangeHelper.getInHeaderString(exchange, CIMHeaderKey.DestinationOdsCode);
-        String requestBody = ExchangeHelper.getInBodyString(exchange);
-
-        SubscriptionManager.getInstance().addSubscription(odsCode, requestBody, exchange.getContext());
+//        String odsCode = ExchangeHelper.getInHeaderString(exchange, CIMHeaderKey.DestinationOdsCode);
+//        String requestBody = ExchangeHelper.getInBodyString(exchange);
+//
+//        SubscriptionManager.getInstance().addSubscription(odsCode, requestBody, exchange.getContext());
     }
 }
